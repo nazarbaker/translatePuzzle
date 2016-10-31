58e77bac7e4a5723953b9f09a2b0fe3327c7679a
Nazar <nazarbaker24@gmail.com>
2016-11-01 00:27:07 +0200

    add mongojs database

--- routes/words.js
+++ routes/words.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://nazar:words@ds139847.mlab.com:39847/words', ['englishwords']);
 
 router.get('/words', function(req, res, next){
-  res.send('Words API');
+  db.englishwords.find(function(err, englishwords){
+    if(err){
+      res.send(err);
+    }
+    res.json(englishwords);
+  })
 });
 
 module.exports = router;
