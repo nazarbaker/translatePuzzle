668c37120da7f8706f43b8492013fe00a461a684
Nazar <nazarbaker24@gmail.com>
2016-11-07 04:02:44 +0200

    add unit test

---
+++ client/app/app.component.spec.ts
@@ -0,0 +1,13 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('App Component', () => {
+
+  it('should Be Truthy', () => {
+    let app = new AppComponent();
+    expect(app).toBeFalsy();
+  });
+
+});
